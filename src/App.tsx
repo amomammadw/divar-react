801d756ddf034e64b8383cfeba6f3a555b29921e
amomammadw82 <67383118+amomammadw82@users.noreply.github.com>
2023-02-02 17:23:19 +0330

    init DIVAR clone

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+
+  return (
+    <div className="text-3xl container">
+      Hey! <br />
+      I'm Gonna Code DIVAR <br />
+      my techs are :
+      <ul>
+        <li>React</li>
+        <li>Typescript</li>
+        <li>TailwindCss</li>
+        <li>React Router</li>
+        <li>Some State Management (idk redux or maybe pinia)</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
